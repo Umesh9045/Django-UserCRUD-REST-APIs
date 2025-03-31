e0d58f8b172043ea05ff1155ada00e651b313043
Umesh9045 <umeshsonawane9045@gmail.com>
2025-03-31 16:23:39 +0530

    created User model and serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()  # Get all users from the database
+    serializer = UserSerializer(users, many=True)  # Convert to JSON
+    # return Response(serializer.data)  # Send response
+    return Response(UserSerializer({'name': "Umesh", 'age': 21}).data)
